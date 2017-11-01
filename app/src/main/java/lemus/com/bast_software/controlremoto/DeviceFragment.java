3f895343f36100c33139ba55af7a5b07c0807919
carlosLemus <notletzero@gmail.com>
2017-11-01 01:46:29 +0100

    Busqueda de dispositivos

--- app/src/main/java/lemus/com/bast_software/controlremoto/DeviceFragment.java
+++ app/src/main/java/lemus/com/bast_software/controlremoto/DeviceFragment.java
@@ -2,10 +2,15 @@ package lemus.com.bast_software.controlremoto;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 
 /**
@@ -18,12 +23,20 @@ public class DeviceFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_device, container, false);
+        View view = inflater.inflate(R.layout.fragment_device, container, false);
+        // Obtenemos la informacion
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)view.findViewById(R.id.bottom_device);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                return true;
+            }
+        });
+        return view;
     }
 
 }
